b4f10a135ae3533e40583ba7cc10f219f9d51ae6
Shirley Startary <shirley.startary@gmail.com>
2023-03-01 13:06:49 -0600

    agrega Link para la navegacion

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../components/Navbar/Navbar";
 
 const About = () => {
     return (
         <div>
+            <Navbar/>
             Esta es la pagina de about
         </div>
     );
