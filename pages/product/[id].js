67cbae810202328dd2f00e35bfcdf9d12266a69d
Shirley Startary <shirley.startary@gmail.com>
2023-03-01 12:23:57 -0600

    agrega ruta dinamicas para los productos

---
+++ pages/product/[id].js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const ProductItem = () => {
+    const {query:{productId}} = useRouter();
+    return (
+        <div>
+            {productId}
+        </div>
+    );
+};
+
+export default ProductItem;
