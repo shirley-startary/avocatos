51fed4e5d22aeedd6f3fe568c1bbcc7828361a55
Shirley Startary <shirley.startary@gmail.com>
2023-03-02 14:41:43 -0600

    Agrega el consumo de un solo aguacate y redirecciona a la vista

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { log } from "console";
 
 const ProductItem = () => {
     const {
         query:{productId},
     } = useRouter();
+    const [product, setProduct] = useState<TProduct>();
+
+    useEffect(()=>{
+        window.fetch(`/api/avo/${productId}`)
+        .then(response => response.json())
+        .then( avo  => setProduct(avo))
+    })
     return (
         <div>
-           Hola: {productId}
+          <h1>{product?.name}</h1>
+          <p>{product?.attributes.description}</p>
         </div>
     );
 };
