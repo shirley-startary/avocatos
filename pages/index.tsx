ab0e744d23f49c8ba5cc4b647a616f9dbf94cc97
Shirley Startary <shirley.startary@gmail.com>
2023-03-02 14:27:43 -0600

    agrega lista de aguacates en la UI consumiendo api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 
-const Home = () => {
+const HomePage = () => {
+    const [productList, setProductList] = useState<TProduct[]>([]);
+
+    useEffect(()=>{
+        window.fetch('/api/avo')
+        .then(response => response.json())
+        .then(({ data }) => setProductList(data))
+    }, [])
+
     return (
         <div>
             <Navbar/>
-            <h1>Hola Mundo con shirley!</h1>
+            <h1>Mis Aguacates</h1>
+            <ul>
+                {productList.map(product => <li><a href={`/product/${product.id}`}>{product.name}</a></li> )}
+                
+            </ul>
         </div>
     )
 }
 
-export default Home;
+export default HomePage;
