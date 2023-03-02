5ccf7a3b9d61c3dfd9b2ba352499f2edfe52426b
Shirley Startary <shirley.startary@gmail.com>
2023-03-02 13:56:10 -0600

    agrega funcion para endpoint de todos los aguacates

--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -1,7 +1,14 @@
+import Database from "@database";
 import { IncomingMessage, ServerResponse } from "http";
 
 const allAvos =async (request:IncomingMessage, response:ServerResponse) => {
-    response.end(JSON.stringify({hello: "world"}));
+    const db = new Database();
+    const allEntries = await db.getAll();
+    const count = allEntries.length;
+
+    response.statusCode = 200;
+    response.setHeader('Content-type', 'application/json');
+    response.end(JSON.stringify({data: allEntries, count}));
 };
 
 export default allAvos;
