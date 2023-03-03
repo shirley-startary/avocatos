10045c8084624a1fd61b87d033b0d422679484f9
Shirley Startary <shirley.startary@gmail.com>
2023-03-02 18:44:53 -0600

    agrega estilos al navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav className={styles.nav}>
+            <h2 className={styles.h2}>Kawai<span className={styles.span}>Avocatos</span></h2>
             <menu>
-                <Link  href="/">
+                <Link href="/" className={styles.a}>
                     Home
                 </Link>
-                <Link href="/about">
+                <Link href="/about" className={styles.a}>
                     About
                 </Link>
             </menu>
