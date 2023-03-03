16cc32dde1e81f25f57339e7e0eb10f17bdbbbee
Shirley Startary <shirley.startary@gmail.com>
2023-03-03 12:40:41 -0600

    se dan estilos a los cards

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -5,12 +5,12 @@ import styles from "./Card.module.css";
 const Card = (props) => {
     return (
         <div className={styles.card}>
-            <figure>
-                <img src="images/avocato1.png" alt="aguacate" />
+            <figure className={styles.imageContainer}>
+                <img src={props.img} alt="aguacate" />
             </figure>
             <div className={styles.contenido}>
                 <h3>{props.name}</h3>
-                <p>Descripcion</p>
+                <p>{props.price}</p>
                 <Link href={props.href}>Detalle</Link>
             </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Card from "@components/Card/Card";
+import styles from './index.module.css';
 
 const HomePage = () => {
     const [productList, setProductList] = useState<TProduct[]>([]);
-
+    
     useEffect(()=>{
         window.fetch('/api/avo')
         .then(response => response.json())
@@ -11,14 +12,16 @@ const HomePage = () => {
     }, [])
 
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Mis Aguacates</h1>
-            <section>
+            <section className={styles.containerCard}>
                 {productList.map(product => 
                     <Card 
                         name={product.name}
                         key={`${product.id}`}
+                        price={product.price}
                         href={`/product/${product.id}`}
+                        img={product.image}
                     />
                 )}
             </section>
